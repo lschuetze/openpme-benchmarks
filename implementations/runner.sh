cda54876bfc0f158101171d0a676b385dfcc636d
Lars Schütze <lars.schuetze@tu-dresden.de>
2021-03-01 12:02:57 +0100

    Fix build

--- implementations/runner.sh
+++ implementations/runner.sh
@@ -4,22 +4,22 @@
 
 case $3 in 
     LennardJonesCL)
-    mpirun -np $1 1_lennard_jones/md_dyn >/dev/null 2>&1
+    mpirun -np $1 ./1_lennard_jones/md_dyn >/dev/null 2>&1
     ;;
 
     LennardJonesVL)
-    mpirun -np $1 1_lennard_jones/md_dyn_vl >/dev/null 2>&1
+    mpirun -np $1 ./1_lennard_jones/md_dyn_vl >/dev/null 2>&1
     ;;
 
     GrayScott)
-    mpirun -np $1 2_gray_scott/gray_scott >/dev/null 2>&1
+    mpirun -np $1 ./2_gray_scott/gray_scott >/dev/null 2>&1
     ;;
 
     VortexInCell)
-    mpirun -np $1 3_vortex_in_cell/vic_petsc >/dev/null 2>&1
+    mpirun -np $1 ./3_vortex_in_cell/vic_petsc >/dev/null 2>&1
     ;;
 
     VortexInCellOpt)
-    mpirun -np $1 3_vortex_in_cell/vic_petsc_opt >/dev/null 2>&1
+    mpirun -np $1 ./3_vortex_in_cell/vic_petsc_opt >/dev/null 2>&1
     ;;
 esac
